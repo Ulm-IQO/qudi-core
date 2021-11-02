633a3a1c8c511fd8a575cc834e2d8b3034dcff2a
Neverhorst <tomek.qudi@gmail.com>
2021-11-02 16:01:22 +0100

    Added minimum required package versions for dependencies

--- setup.py
+++ setup.py
@@ -46,21 +46,21 @@ unix_dep = [
 ]
 
 windows_dep = [
-    'wheel',
-    'cycler',
-    'entrypoints',
-    'fysom',
-    'GitPython',
-    'jupyter',
-    'jupytext',
-    'lmfit',
-    'matplotlib',
-    'numpy',
-    'pyqtgraph',
-    'PySide2',
-    'rpyc',
-    'ruamel.yaml',
-    'scipy',
+    'wheel>=0.37.0',
+    'cycler>=0.10.0',
+    'entrypoints>=0.3',
+    'fysom>=2.1.6',
+    'GitPython>=3.1.24',
+    'jupyter>=1.0.0',
+    'jupytext>=1.13.0',
+    'lmfit>=1.0.3',
+    'matplotlib>=3.4.3',
+    'numpy>=1.21.3',
+    'pyqtgraph>=0.12.3',
+    'PySide2==5.15.2',
+    'rpyc>=5.0.1',
+    'ruamel.yaml>=0.17.16',
+    'scipy>=1.7.1',
 ]
 
 with open('VERSION', 'r') as file:
