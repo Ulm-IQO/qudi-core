8e31da31b75518d3397f6fc00a45eba82c653aa6
Neverhorst <tomek.qudi@gmail.com>
2022-03-10 09:27:40 +0100

    WIP

--- src/qudi/core/config/section.py
+++ src/qudi/core/config/section.py
@@ -24,7 +24,7 @@ If not, see <https://www.gnu.org/licenses/>.
 __all__ = ['ConfigurationSection']
 
 import re
-from typing import List, Any, Mapping, Optional
+from typing import List, Any, Mapping, Optional, Union
 
 from .items import *
 
@@ -33,27 +33,25 @@ class ConfigurationSection:
     """
     """
 
-    def __init__(self, config: Optional[Mapping[str, Any]] = None) -> None:
+    def __init__(self, **kwargs) -> None:
         super().__init__()
-        if config is not None:
-            self.update()
-            for key, value in config.items():
-                try:
-                    setattr(self, key, value)
-                except AttributeError:
-                    pass
+        for key, value in kwargs.items():
+            try:
+                setattr(self, key, value)
+            except AttributeError:
+                pass
 
     @classmethod
-    def _is_config_item(cls, name) -> bool:
+    def is_config_item(cls, name) -> bool:
         return isinstance(cls.__dict__.get(name.lstrip('_'), None), AbstractConfigurationItem)
 
     @classmethod
-    def _config_item_names(cls) -> List[str]:
+    def config_item_names(cls) -> List[str]:
         return [name for name, attr in cls.__dict__.items() if
                 isinstance(attr, AbstractConfigurationItem)]
 
     def __setattr__(self, name, value):
-        if self._is_config_item(name) or hasattr(self, name) or hasattr(self, name.lstrip('_')):
+        if self.is_config_item(name) or hasattr(self, name) or hasattr(self, name.lstrip('_')):
             super().__setattr__(name, value)
         else:
             raise AttributeError(f'No "{self.__class__.__name__}" member with name "{name}". '
@@ -63,12 +61,12 @@ class ConfigurationSection:
         raise AttributeError(f'Can not delete "{self.__class__.__name__}" members')
 
     def __getitem__(self, key):
-        if not key.startswith('_') and self._is_config_item(key):
+        if not key.startswith('_') and self.is_config_item(key):
             return getattr(self, key)
         raise KeyError(f'No "{self.__class__.__name__}" item with name "{key}"')
 
     def __setitem__(self, key, value):
-        if not key.startswith('_') and self._is_config_item(key):
+        if not key.startswith('_') and self.is_config_item(key):
             return setattr(self, key, value)
         raise KeyError(f'No "{self.__class__.__name__}" item with name "{key}". '
                        f'Can not set new "{self.__class__.__name__}" items.')
@@ -77,21 +75,21 @@ class ConfigurationSection:
         raise KeyError(f'Can not delete "{self.__class__.__name__}" items')
 
     def __len__(self):
-        return len(self._config_item_names())
+        return len(self.config_item_names())
 
     def __iter__(self):
         return self.keys()
 
     def keys(self):
-        for key in self._config_item_names():
+        for key in self.config_item_names():
             yield key
 
     def values(self):
-        for key in self._config_item_names():
+        for key in self.config_item_names():
             yield getattr(self, key)
 
     def items(self):
-        for key in self._config_item_names():
+        for key in self.config_item_names():
             yield key, getattr(self, key)
 
     def get(self, key, default=None):
@@ -109,7 +107,7 @@ class ConfigurationSection:
         elif kwargs:
             mapping = mapping.copy()
             mapping.update(kwargs)
-        allowed_keys = self._config_item_names()
+        allowed_keys = self.config_item_names()
         if not all(key in allowed_keys for key in mapping):
             raise KeyError(f'Invalid "{self.__class__.__name__}" item encountered.')
         for key, value in mapping.items():
@@ -130,34 +128,41 @@ class GlobalConfiguration(ConfigurationSection):
     default_data_dir = StrConfigurationItem()
     extension_paths = StrListConfigurationItem()
 
-    def __init__(self, config: Optional[Mapping[str, Any]] = None) -> None:
+    def __init__(self, **kwargs) -> None:
         self.startup_modules = list()
-        self.remote_modules_server = {'address': 'localhost',
-                                      'port'   : 12345}
+        self.remote_modules_server = {'address': 'localhost', 'port'   : 12345}
         self.namespace_server_port = 18861
-        self.force_remote_calls_by_value = True
+        self.force_remote_calls_by_value = False
         self.hide_manager_window = False
         self.stylesheet = 'qdark.qss'
         self.daily_data_dirs = True
         self.default_data_dir = None
         self.extension_paths = None
-        super().__init__(config=config)
+        super().__init__(**kwargs)
 
 
 class LocalModuleConfiguration(ConfigurationSection):
     """
     """
-    module_class = StrConfigurationItem()
+
+    @staticmethod
+    def validate_module_class(module_class: str) -> None:
+        if re.match(r'^\w+(\.\w+)*$', module_class) is None:
+            raise ValueError('qudi module config "module_class" must be non-empty str containing a '
+                             'valid Python "module.Class"-like path, e.g. '
+                             '"my_module.my_submodule.MyClass"')
+
+    module_class = StrConfigurationItem(validator=validate_module_class)
     allow_remote = BoolConfigurationItem()
     connect = MappingConfigurationItem()
     options = MappingConfigurationItem()
 
-    def __init__(self, config: Optional[Mapping[str, Any]] = None) -> None:
+    def __init__(self, **kwargs) -> None:
         self.module_class = None
         self.allow_remote = False
         self.connect = None
         self.options = None
-        super().__init__(config=config)
+        super().__init__(**kwargs)
 
 
 class RemoteModuleConfiguration(ConfigurationSection):
@@ -167,26 +172,25 @@ class RemoteModuleConfiguration(ConfigurationSection):
     keyfile = StrConfigurationItem()
     certfile = StrConfigurationItem()
 
-    def __init__(self, config: Optional[Mapping[str, Any]] = None) -> None:
+    def __init__(self, **kwargs) -> None:
         self.remote_url = None
         self.keyfile = None
         self.certfile = None
-        super().__init__(config=config)
+        super().__init__(**kwargs)
 
 
 class ModulesConfiguration:
     """
     """
-    _module_name_regex = re.compile(r'^\w+(\s\w+)*$')
+    _allow_remote = True
+    _module_base = ''
 
-    def __init__(self, module_base: str, config: Optional[Mapping[str, Mapping]] = None) -> None:
-        if module_base not in ['gui', 'logic', 'hardware']:
-            raise ValueError('"module_base" must be one of ["gui", "logic", "hardware"]')
-        self._module_base = module_base
-        self._modules = dict()
+    def __init__(self, config: Optional[Mapping[str, Mapping]] = None) -> None:
+        self._local_modules = dict()
+        self._remote_modules = dict()
         if config is not None:
             for key, module_dict in config.items():
-                if 'remote_url' in module_dict:
+                if 'remote_url' in module_dict and not 'module_class' in module_dict:
                     self[key] = RemoteModuleConfiguration(module_dict)
                 else:
                     self[key] = LocalModuleConfiguration(module_dict)
@@ -197,16 +201,28 @@ class ModulesConfiguration:
                          allow_remote: Optional[bool] = None,
                          connect: Optional[Mapping[str, str]] = None,
                          options: Optional[Mapping[str, Any]] = None) -> None:
-        self.validate_name(name)
+        self[name] = LocalModuleConfiguration(module_class=module_class,
+                                              allow_remote=allow_remote,
+                                              connect=connect,
+                                              options=options)
 
-    @classmethod
-    def validate_name(cls, name: str) -> None:
-        if cls._module_name_regex.match(name) is None:
+    def __getitem__(self, key: str) -> Union[LocalModuleConfiguration, RemoteModuleConfiguration]:
+        module_config = self._local_modules.get(key, None)
+        if module_config is None:
+            module_config = self._remote_modules.get(key, None)
+        if module_config is None:
+            raise KeyError(f'Local qudi {self._module_base} module with name "{name}" already '
+                             f'configured.')
+
+    def validate_name(self, name: str) -> None:
+        if re.match(r'^\w+(\s\w+)*$', name) is None:
             raise ValueError('qudi module config name must be non-empty str containing only '
                              'unicode word characters and spaces.')
+        if name in self._local_modules:
+            raise ValueError(f'Local qudi {self._module_base} module with name "{name}" already '
+                             f'configured.')
+        if name in self._remote_modules:
+            raise ValueError(f'Remote qudi {self._module_base} module with name "{name}" already '
+                             f'configured.')
 
-    @classmethod
-    def validate_module_class(cls, module_class: str) -> None:
-        if cls._module_name_regex.match(name) is None:
-            raise ValueError('qudi module config name must be non-empty str containing only '
-                             'unicode word characters and spaces.')
+
