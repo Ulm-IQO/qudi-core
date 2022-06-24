96e29670fb1cdf81f0c832b0fd0313b072a3782f
Neverhorst <tomek.qudi@gmail.com>
2022-06-24 18:29:36 +0200

    - Got rid of misleading mixin scheme in `qudi.core.config` - Shorter naming of `Configuration` methods - Code cleanup

--- src/qudi/core/application.py
+++ src/qudi/core/application.py
@@ -33,9 +33,8 @@ from qudi.core.logger import init_rotating_file_handler, init_record_model_handl
 from qudi.core.logger import get_logger, set_log_level
 from qudi.util.paths import get_main_dir, get_default_log_dir
 from qudi.util.mutex import Mutex
-from qudi.util.yaml import ParserError
 from qudi.util.colordefs import QudiMatplotlibStyle
-from qudi.core.config import Configuration, ValidationError
+from qudi.core.config import Configuration, ValidationError, ParserError
 from qudi.core.watchdog import AppWatchdog
 from qudi.core.modulemanager import ModuleManager
 from qudi.core.threadmanager import ThreadManager
@@ -116,7 +115,7 @@ class Qudi(QtCore.QObject):
         # Load configuration from disc if possible
         self.configuration = Configuration()
         try:
-            self.configuration.load_config(config_file, set_default=True)
+            self.configuration.load(config_file, set_default=True)
         except ValueError:
             self.log.info('No qudi configuration file specified. Using empty default config.')
         except (ValidationError, ParserError):
--- src/qudi/core/config/config.py
+++ src/qudi/core/config/config.py
@@ -21,21 +21,132 @@ You should have received a copy of the GNU Lesser General Public License along w
 If not, see <https://www.gnu.org/licenses/>.
 """
 
-__all__ = ['Configuration']
+__all__ = ['Configuration', 'ValidationError', 'ParserError']
 
 import copy
+from numbers import Number
 from PySide2 import QtCore
-from typing import Any, Optional, MutableMapping, Union
+from typing import Mapping, Optional, Union, Sequence, Set, List, MutableMapping, Any
 from collections.abc import MutableMapping as _MutableMapping
 from qudi.core.meta import ABCQObjectMeta as _ABCQObjectMeta
 
+from .validator import ValidationError
 from .validator import validate_config as _validate_config
-from ._mixins import ModuleConfigMixin as _ModuleConfigMixin
-from ._mixins import FileHandlerMixin as _FileHandlerMixin
+from .validator import validate_local_module_config as _validate_local_module_config
+from .validator import validate_remote_module_config as _validate_remote_module_config
+from .file_handler import ParserError
+from .file_handler import FileHandlerBase as _FileHandlerBase
 
 
-class Configuration(_FileHandlerMixin,
-                    _ModuleConfigMixin,
+class _ModuleConfigInterface:
+    """ Mixin for adding and removing qudi modules from the qudi configuration.
+    """
+    _OptionType = Union[Sequence, Mapping, Set, Number, str]
+
+    def add_local_module(self,
+                         base: str,
+                         name: str,
+                         module_class: str,
+                         allow_remote: Optional[bool] = None,
+                         connect: Optional[Mapping[str, str]] = None,
+                         options: Optional[Mapping[str, _OptionType]] = None) -> None:
+        if self.module_configured(name):
+            raise KeyError(f'Module with name "{name}" already configured')
+        self.validate_module_base(base)
+        module_config = {'module.Class': module_class}
+        if allow_remote is not None:
+            module_config['allow_remote'] = allow_remote
+        if connect is not None:
+            module_config['connect'] = copy.copy(connect)
+        if options is not None:
+            module_config['options'] = copy.deepcopy(options)
+        _validate_local_module_config(module_config)
+        new_config = self.config_map
+        new_config[base][name] = module_config
+        self.set_config(new_config)
+
+    def add_remote_module(self,
+                          base: str,
+                          name: str,
+                          remote_url: str,
+                          certfile: Optional[str] = None,
+                          keyfile: Optional[str] = None) -> None:
+        if self.module_configured(name):
+            raise KeyError(f'Module with name "{name}" already configured')
+        self.validate_module_base(base)
+        module_config = {'remote_url': remote_url}
+        if certfile is not None:
+            module_config['certfile'] = certfile
+        if keyfile is not None:
+            module_config['keyfile'] = keyfile
+        _validate_remote_module_config(module_config)
+        new_config = self.config_map
+        new_config[base][name] = module_config
+        self.set_config(new_config)
+
+    def rename_module(self, old_name: str, new_name: str) -> None:
+        if old_name == new_name:
+            return
+        if not self.module_configured(old_name):
+            raise KeyError(f'No module with name "{old_name}" configured')
+        if self.module_configured(new_name):
+            raise KeyError(f'Module with name "{new_name}" already configured')
+
+        new_config = self.config_map
+        for base in ['gui', 'logic', 'hardware']:
+            try:
+                module_config = new_config[base].pop(old_name)
+            except KeyError:
+                continue
+            else:
+                new_config[base][new_name] = module_config
+                self.set_config(new_config)
+                return
+
+    def remove_module(self, name: str) -> None:
+        new_config = self.config_map
+        for base in ['gui', 'logic', 'hardware']:
+            try:
+                del new_config[base][name]
+            except KeyError:
+                continue
+            else:
+                self.set_config(new_config)
+                return
+        raise KeyError(f'No module with name "{name}" configured')
+
+    def module_configured(self, name: str) -> bool:
+        """ Checks if a module with given name is present in current configuration """
+        return name in self._config['gui'] or name in self._config['logic'] or name in self._config[
+            'hardware']
+
+    def module_config(self, name: str) -> MutableMapping[str, Any]:
+        for base in ['gui', 'logic', 'hardware']:
+            try:
+                return copy.deepcopy(self._config[base][name])
+            except KeyError:
+                pass
+        raise KeyError(f'No module with name "{name}" configured')
+
+    def is_remote_module(self, name):
+        return 'remote_url' in self.get_module_config(name)
+
+    def is_local_module(self, name):
+        return 'module.Class' in self.get_module_config(name)
+
+    @property
+    def module_names(self) -> List[str]:
+        """ List of the currently configured module names. """
+        return [*self._config['gui'], *self._config['logic'], *self._config['hardware']]
+
+    @staticmethod
+    def validate_module_base(base: str) -> None:
+        if base not in ['gui', 'logic', 'hardware']:
+            raise ValueError('qudi module base must be one of ["gui", "logic", "hardware"]')
+
+
+class Configuration(_FileHandlerBase,
+                    _ModuleConfigInterface,
                     _MutableMapping,
                     QtCore.QObject,
                     metaclass=_ABCQObjectMeta):
@@ -102,42 +213,42 @@ class Configuration(_FileHandlerMixin,
                 yield key
 
     def __len__(self) -> int:
-        return len(self._config) + len(self._config['global']) - 1
+        return max(0, len(self._config) + len(self._config['global']) - 1)
 
-    def load_config(self, file_path=None, set_default=False):
+    def load(self, file_path=None, set_default=False):
         file_path = self._file_path if file_path is None else file_path
         # Try to restore last loaded config file path if possible
         if file_path is None:
             try:
-                file_path = self.get_saved_config_path()
+                file_path = self.get_saved_path()
             except FileNotFoundError:
                 try:
-                    file_path = self.get_default_config_path()
+                    file_path = self.get_default_path()
                 except FileNotFoundError:
                     pass
         if file_path is None:
             raise ValueError('No file path defined for configuration to load')
 
         # Load YAML file from disk.
-        config = self._load_config_file(file_path)
+        config = self._load(file_path)
         # validate config, add missing default values and set as current config.
         old_path = self._file_path
         try:
             self._file_path = file_path
             self.set_config(config)
-        except:
+        except ValidationError:
             self._file_path = old_path
             raise
 
         # Write current config file path to load.cfg in AppData if requested
         if set_default:
-            self.set_default_config_path(file_path)
+            self.set_default_path(file_path)
 
-    def dump_config(self, file_path=None):
+    def dump(self, file_path=None):
         file_path = self._file_path if file_path is None else file_path
         if file_path is None:
             raise ValueError('No file path defined for qudi configuration to dump into')
         config = self.config_map
         _validate_config(config)
-        self._dump_config_file(file_path, config)
+        self._dump(file_path, config)
         self._file_path = file_path
