Added one basic test for the rodriguez rotation and fixed imports.

src/qudi/util/basis_transformations/basis_transformation.py
@@ -1,6 +1,6 @@
 import numpy as np
-from basis_transformation_helpers import assert_np_square_mat, gram_schmidt_columns, \
-computational_basis
+from qudi.util.basis_transformations.basis_transformation_helpers import assert_np_square_mat, \
+    gram_schmidt_columns, computational_basis
 from qudi.util.math import normalize
 
 def basis_from_points(points:np.ndarray)->np.ndarray:

src/qudi/util/basis_transformations/basis_transformation_tests.py
@@ -5,7 +5,7 @@ from qudi.util.math import normalize
 import numpy as np
 
 from basis_transformation_helpers import computational_basis, random_vectors, scalar_product_along_axis
-from basis_transformation import is_orthogonal_basis 
+from basis_transformation import is_orthogonal_basis, compute_rotation_mat_rodriguez 
 
 class TransformTests(unittest.TestCase):
     def __init__(self, *args, **kwargs):
@@ -35,6 +35,21 @@ class TransformTests(unittest.TestCase):
 
     def test_basis_from_points(self):
         print('Basis from points test')
+
+    def test_rodriguez_rotation(self):
+        """
+        Just some functionality tests of the computation of
+        the 3D rotation matrix from support vectors.
+        """
+        # If all points are in the z-plane then the resulting transformation should not
+        # change the z position of any point
+        p0, p1, p2 = np.array([1, 0, 0]),  np.array([0, 1, 0]), np.array([3,2,0])
+        rot_mat = compute_rotation_mat_rodriguez(p0, p1, p2)
+        p_rnd = np.random.rand(3)
+        z_rnd = p_rnd[2]
+        p_rnd_transformed = np.matmul(rot_mat, p_rnd)
+        z_rnd_transformed = p_rnd_transformed[2]
+        self.assertTrue(z_rnd == z_rnd_transformed)
 
 if __name__ == '__main__':
  #   my_test = TransformTests()