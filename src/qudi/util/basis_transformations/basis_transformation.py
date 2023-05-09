c3a64cce6afa8f260804e69e86a1b9eeb4ba4964
geegee <wgolff@googlemail.com>
2023-05-09 12:06:26 +0200

    Added a function to determine which axes are changing.

--- src/qudi/util/basis_transformations/basis_transformation.py
+++ src/qudi/util/basis_transformations/basis_transformation.py
@@ -46,8 +46,25 @@ def point_in_new_basis_shifted(components:np.ndarray, old_basis:np.ndarray, new_
     new_components = point_in_new_basis(components, old_basis, new_basis)
     return new_components + shift
 
+def det_changing_axes(points: np.ndarray) -> np.ndarray:
+    num_axes = len(points[0])
+    axes_changing_p = np.zeros(num_axes, dtype=np.bool_)
+    # substract each
+    for axis in range(num_axes):
+        elements = points[:, axis]
+        for ii, element in enumerate(points[:, axis]):
+            d_elements = element - elements[ii+1:]
+            if np.any(d_elements > 0):
+                axes_changing_p[axis] = True
+                break
+    return axes_changing_p
+
+def compute_reduced_vectors(points: np.ndarray) -> np.ndarray:
+    axes_changing_p = det_changing_axes(points)
+    return points[:, axes_changing_p]
+
 # code for 3D rotation matrix, roughly following chat GPT
-def compute_rotation_mat_rodriguez(v0:np.ndarray, v1:np.ndarray, v2:np.ndarray)->np.ndarray:
+def compute_rotation_mat_rodriguez(v0: np.ndarray, v1: np.ndarray, v2: np.ndarray) -> np.ndarray:
     if len(v0) != 3 or len(v1) != 3 or len(v2) != 3:
         raise ValueError('The support vectors should have a length of 3.')
     s0 = v1 - v0
