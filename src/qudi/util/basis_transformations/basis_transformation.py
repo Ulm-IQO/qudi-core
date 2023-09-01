1175af06ea097d2a7b3f43e69d36e3f6192ae7c4
Timo <timo.joas@uni-ulm.de>
2023-09-01 16:28:02 +0200

    rename functions, add documentation

--- src/qudi/util/basis_transformations/basis_transformation.py
+++ src/qudi/util/basis_transformations/basis_transformation.py
@@ -2,7 +2,15 @@ import numpy as np
 
 from qudi.util.math import normalize
 
-def det_changing_axes(points: np.ndarray) -> np.ndarray:
+# todo: integrate in math.py or linear_transform.py
+
+def find_changing_axes(points: np.ndarray) -> np.ndarray:
+    """
+    From a set of column vectors, find the axes which do not have the same coordinate for all vectors.
+
+    :param points: each row is a vector
+    :return: array of the indices of the changing axes
+    """
     num_axes = len(points[0])
     axes_changing_p = np.zeros(num_axes, dtype=np.bool_)
     # substract each
@@ -16,15 +24,33 @@ def det_changing_axes(points: np.ndarray) -> np.ndarray:
     return axes_changing_p
 
 def compute_reduced_vectors(points: np.ndarray) -> np.ndarray:
-    axes_changing_p = det_changing_axes(points)
+    """
+    From a set of column vectors, keep only the columns (= axes) that are not constant coordinates for all vectors.
+
+    :param points: each row is a vector
+    :return: reduced array
+    """
+    axes_changing_p = find_changing_axes(points)
     return points[:, axes_changing_p]
 
-def compute_rotation_mat_rodriguez(v0: np.ndarray, v1: np.ndarray, v2: np.ndarray) -> np.ndarray:
+def compute_rotation_matrix_to_plane(v0: np.ndarray, v1: np.ndarray, v2: np.ndarray, ez=[0,0,1]) -> np.ndarray:
+    """
+    Find the rotation matrix that transforms a plane given by 3 support vectors onto the z plane.
+    This rotation will be around the origin of the coordinate system.
+    Optionally, define the target plane by it's normal vector.
+    See the math here: https://en.wikipedia.org/wiki/Rodrigues'_rotation_formula
+
+    :param v0: Support vector 0
+    :param v1: Support vector 1
+    :param v2: Support vector 2
+    :param ez: Normal vector defining the target plane. By default this is the z plane.
+    :return: 3x3 rotation matrix
+    """
     if len(v0) != 3 or len(v1) != 3 or len(v2) != 3:
         raise ValueError('The support vectors should have a length of 3.')
     s0 = v1 - v0
     s1 = v2 - v0
-    ez = np.asarray([0,0,1])
+    ez = np.asarray(ez)
 
     normal_plane_vec = normalize(np.cross(s0, s1))[0]
     rot_axis = normalize(np.cross(normal_plane_vec, ez))[0]
@@ -36,5 +62,4 @@ def compute_rotation_mat_rodriguez(v0: np.ndarray, v1: np.ndarray, v2: np.ndarra
     if theta > np.pi/2 or theta < -np.pi/2:
         theta = -(np.pi-theta)
 
-    # See the math here: https://en.wikipedia.org/wiki/Rodrigues'_rotation_formula
     return np.eye(3) + np.sin(theta) * k_mat + (1 - np.cos(theta)) * np.matmul(k_mat, k_mat)
