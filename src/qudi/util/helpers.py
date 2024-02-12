513c9633deefb5af6617ee3d85be0c8103fada31
Neverhorst <tomek.qudi@gmail.com>
2024-02-12 22:22:15 +0100

    - Added helper methods `call_slot_from_native_thread`, `current_is_native_thread` and `current_is_main_thread` to `qudi.util.helpers` for easy handling of `QObject` thread affinity

--- src/qudi/util/helpers.py
+++ src/qudi/util/helpers.py
@@ -19,12 +19,14 @@ If not, see <https://www.gnu.org/licenses/>.
 
 __all__ = ['csv_2_list', 'in_range', 'is_complex', 'is_complex_type', 'is_float', 'is_float_type',
            'is_integer', 'is_integer_type', 'is_number', 'is_number_type', 'is_string',
-           'is_string_type', 'iter_modules_recursive', 'natural_sort', 'str_to_number']
+           'is_string_type', 'iter_modules_recursive', 'natural_sort', 'str_to_number',
+           'call_slot_from_native_thread', 'current_is_native_thread', 'current_is_main_thread']
 
 import re
 import os
 import pkgutil
 import numpy as np
+from PySide2 import QtCore
 from typing import Union, Optional, Iterable, List, Any, Type, Tuple, Callable
 
 _RealNumber = Union[int, float]
@@ -81,6 +83,35 @@ def natural_sort(iterable: Iterable[Any]) -> List[Any]:
         return sorted(iterable, key=lambda key: [conv(i) for i in re.split(r'(\d+)', key)])
     except:
         return sorted(iterable)
+
+
+def current_is_native_thread(obj: QtCore.QObject) -> bool:
+    """ Check if the current thread is the native thread of given QObject """
+    return QtCore.QThread.currentThread() == obj.thread()
+
+
+def current_is_main_thread() -> bool:
+    """ Check if the current thread is the Qt applications main thread. Only works if the main
+    event loop is running.
+    """
+    main = QtCore.QCoreApplication.instance()
+    if main is None:
+        raise RuntimeError('No Qt main event loop running. Unable to retrieve main thread.')
+    return QtCore.QThread.currentThread() == main.thread()
+
+
+def call_slot_from_native_thread(obj: QtCore.QObject,
+                                 slot_name: str,
+                                 blocking: Optional[bool] = True) -> None:
+    """ Calls a slot with given name and without arguments on given QObject.
+    Will raise RuntimeError if the current thread is already the native thread. """
+    if current_is_native_thread(obj):
+        raise RuntimeError('Current thread is the native thread!')
+    QtCore.QMetaObject.invokeMethod(
+        obj,
+        slot_name,
+        QtCore.Qt.BlockingQueuedConnection if blocking else QtCore.Qt.QueuedConnection
+    )
 
 
 def is_number(test_value: Any) -> bool:
