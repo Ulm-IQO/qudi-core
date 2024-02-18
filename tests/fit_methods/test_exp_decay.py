c8a2d64555db934d9ae380ef4f26c3fccef12b43
Prithvi Gundlapalli <prithvi.gundlapalli@uni-ulm.de>
2024-02-18 09:29:13 +0100

    Generalising test more by permuting pytest parameters. To discuss with #10, I'm not sure if I understand the fit logic. We use lmfit to do the fitting but have our own methods to generate the initial guesses?

--- tests/fit_methods/test_exp_decay.py
+++ tests/fit_methods/test_exp_decay.py
@@ -27,37 +27,49 @@ from qudi.util.fit_models.exp_decay import ExponentialDecay
 
 
 SEED = 42
-NUM_TESTS = 100
-NUM_X_VALUES = 1000
-TOLERANCE = 0.05
+NUM_TESTS = 10
+NUM_X_VALUES = 1_000
+RTOL = 0.05
+ATOL = 1e-3
+SMALL_VALUE_ATOL = 1e-5
 
 rng = np.random.default_rng(seed=SEED)
 
 
 @pytest.fixture
 def generate_params():
-    offset = rng.uniform(1e2, 1e3)
-    amplitude = rng.uniform(1e1, 1e3)  # If this is too small, the fit errors become large
+    amplitude = rng.uniform(
+        1e1, 1e3
+    )  # If this is too small, the fit errors become large
     decay = rng.uniform(1, 10)
-    stretch = rng.random()
     x_values = np.linspace(0, 100, NUM_X_VALUES)
     noise = rng.normal(0, 0.1, x_values.shape)
 
-    yield offset, amplitude, decay, stretch, x_values, noise
+    yield amplitude, decay, x_values, noise
 
 
 @pytest.mark.parametrize("generate_params", range(NUM_TESTS), indirect=True)
-def test_exp_decay(generate_params):
-    offset, amplitude, decay, stretch, x_values, noise = generate_params
+@pytest.mark.parametrize("stretch", [1.0] + list(rng.uniform(0, 10, NUM_TESTS - 1)))
+@pytest.mark.parametrize("offset", [0.0] + list(rng.uniform(1e2, 1e3, NUM_TESTS - 1)))
+def test_exp_decay(generate_params, stretch, offset):
+    amplitude, decay, x_values, noise = generate_params
 
-    stretch = 1
     y_values = noise + ExponentialDecay().eval(
         x=x_values, offset=offset, amplitude=amplitude, decay=decay, stretch=stretch
     )
 
     fit_model = ExponentialDecay()
-    estimate = fit_model.guess(y_values, x_values)
-    estimate["stretch"].set(vary=False, value=stretch)
+    if stretch == 1.0:
+        if offset == 0.0:
+            estimate = fit_model.estimate_decay_no_offset(y_values, x_values)
+        else:
+            estimate = fit_model.estimate_decay(y_values, x_values)
+    else:
+        if offset == 0.0:
+            estimate = fit_model.estimate_stretched_decay_no_offset(y_values, x_values)
+        else:
+            estimate = fit_model.estimate_stretched_decay(y_values, x_values)
+
     fit_result = fit_model.fit(data=y_values, x=x_values, **estimate)
 
     params_ideal = {
@@ -68,36 +80,15 @@ def test_exp_decay(generate_params):
     }
     for name, ideal_val in params_ideal.items():
         fit_val = fit_result.best_values[name]
-        relative_err = abs(abs(fit_val - ideal_val) / ideal_val)
-        # msg = f'Exp. decay fit parameter "{name}" has relative error {relative_err * 100:.2f}% (Limit: {TOLERANCE * 100:.2f}%)'
-        msg = f'Actual: {fit_val}, Ideal: {ideal_val}, {name}, Relative error: {relative_err * 100:.2f}%, Absolute error: {abs(fit_val - ideal_val)}'
-        assert relative_err <= TOLERANCE, msg
+        if ideal_val >= SMALL_VALUE_ATOL:
+            relative_err = abs(abs(fit_val - ideal_val) / ideal_val)
+            msg = f'Exp. decay fit parameter "{name}" has relative error {relative_err * 100:.2f}% (Limit: {RTOL * 100:.2f}%)'
+            assert relative_err <= RTOL, msg
+        else:
+            absolute_err = abs(fit_val - ideal_val)
+            msg = f'Exp. decay fit parameter "{name}" has absolute error {absolute_err:.5f}% (Limit: {ATOL * 100:.2f})'
+            assert absolute_err <= ATOL, msg
 
 
 if __name__ == "__main__":
     pytest.main()
-
-# @pytest.mark.parametrize("stretch", [0.1 + rng.random() * 3.9])
-# def test_stretched_exp_decay(setup_values, stretch):
-#     offset, amplitude, decay, _, x_values, noise = setup_values
-
-#     y_values = noise + (offset + amplitude * np.exp(-((x_values / decay) ** stretch)))
-
-#     fit_model = ExponentialDecay()
-#     fit_result = fit_model.fit(
-#         data=y_values, x=x_values, **fit_model.guess(y_values, x_values)
-#     )
-
-#     params_ideal = {
-#         "offset": offset,
-#         "amplitude": amplitude,
-#         "decay": decay,
-#         "stretch": stretch,
-#     }
-#     for name, ideal_val in params_ideal.items():
-#         diff = abs(fit_result.best_values[name] - ideal_val)
-#         tolerance = abs(ideal_val * 0.05)
-#         msg = 'Stretched exp. decay fit parameter "{0}" not within {1:.2%} tolerance'.format(
-#             name, 0.05
-#         )
-#         assert diff <= tolerance
